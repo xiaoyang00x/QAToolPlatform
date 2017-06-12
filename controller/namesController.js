8b2759c847fde82f2a13642812a881798e2dea52
Alex Yang <yangy@shicaidai.com>
2017-06-12 17:16:48 +0800

    fix

--- controller/namesController.js
+++ controller/namesController.js
@@ -1,6 +1,8 @@
 angular.module('myApp', []).controller('interfaceName', function ($scope, $http) {
     var url = 'http://' + window.serverIP + ':3000/postman/getAllInterfaceList';// URL where the Node.js server is running
     $http.get(url).success(function (data) {
+        if(data.indexOf("404")>0)
+            alert(data);
         $scope.list = data
     });
 });
